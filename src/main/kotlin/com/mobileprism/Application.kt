816a09fa31a4c714761d50ffe622620c645743da
Merkost <KonstantinLikes@gmail.com>
2022-05-30 20:13:58 +1000

    Initial commit

---
+++ src/main/kotlin/com/mobileprism/Application.kt
@@ -0,0 +1,12 @@
+package com.mobileprism
+
+import io.ktor.server.engine.*
+import io.ktor.server.cio.*
+import com.mobileprism.plugins.*
+
+fun main() {
+    embeddedServer(CIO, port = 8080, host = "0.0.0.0") {
+        configureRouting()
+        configureSerialization()
+    }.start(wait = true)
+}
