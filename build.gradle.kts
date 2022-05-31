e85a392a8ec953141aa2a595674514937c4dca6b
Merkost <KonstantinLikes@gmail.com>
2022-05-31 22:02:25 +1000

    exposed added

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,12 @@
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
+val exposed_version: String by project
 
 plugins {
     application
     kotlin("jvm") version "1.6.21"
-                id("org.jetbrains.kotlin.plugin.serialization") version "1.6.21"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.6.21"
 }
 
 group = "com.mobileprism"
@@ -26,6 +27,14 @@ dependencies {
     implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
+
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
+
+    implementation("org.postgresql:postgresql:42.3.4")
+    implementation("com.zaxxer:HikariCP:5.0.1")
+
     implementation("io.ktor:ktor-server-cio-jvm:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
--- src/main/kotlin/com/mobileprism/Application.kt
+++ src/main/kotlin/com/mobileprism/Application.kt
@@ -1,11 +1,21 @@
 package com.mobileprism
 
+import com.mobileprism.login.configureLoginRouting
 import io.ktor.server.engine.*
 import io.ktor.server.cio.*
 import com.mobileprism.plugins.*
+import org.jetbrains.exposed.sql.Database
 
 fun main() {
+    Database.connect(
+        "jdbc:postgresql://localhost:5432/fishingnotes",
+        driver = "org.postgresql.Driver",
+        user = "postgres",
+        password = "1234"
+    )
+
     embeddedServer(CIO, port = 8080, host = "0.0.0.0") {
+        configureLoginRouting()
         configureRouting()
         configureSerialization()
     }.start(wait = true)
