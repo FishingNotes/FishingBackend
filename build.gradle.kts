ddc99f708edacdcc4e34ee1014aa662e40fcdeea
Merkost <KonstantinLikes@gmail.com>
2022-06-01 20:11:31 +1000

    Added logging

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,7 @@ dependencies {
     implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-call-logging:$ktor_version")
 
     implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
--- src/main/kotlin/com/mobileprism/Application.kt
+++ src/main/kotlin/com/mobileprism/Application.kt
@@ -1,9 +1,12 @@
 package com.mobileprism
 
-import com.mobileprism.login.configureLoginRouting
+import com.mobileprism.database.features.login.configureLoginRouting
+import com.mobileprism.database.features.register.configureRegisterRouting
 import io.ktor.server.engine.*
 import io.ktor.server.cio.*
 import com.mobileprism.plugins.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.callloging.*
 import org.jetbrains.exposed.sql.Database
 
 fun main() {
@@ -15,7 +18,9 @@ fun main() {
     )
 
     embeddedServer(CIO, port = 8080, host = "0.0.0.0") {
+        install(CallLogging)
         configureLoginRouting()
+        configureRegisterRouting()
         configureRouting()
         configureSerialization()
     }.start(wait = true)
