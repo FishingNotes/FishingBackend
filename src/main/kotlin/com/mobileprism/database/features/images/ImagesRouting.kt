2fe622322a8fc400e7a8b721d69be03148da9817
Merkost <KonstantinLikes@gmail.com>
2022-07-10 23:07:30 +1000

    Images migrated to UserImages

---
+++ src/main/kotlin/com/mobileprism/database/features/images/ImagesRouting.kt
@@ -0,0 +1,29 @@
+package com.mobileprism.database.features.images
+
+import io.ktor.server.application.*
+import io.ktor.server.routing.*
+
+
+fun Routing.imagesRouting() {
+    route("/images") {
+        get {
+            UserImagesController().getAllImages(call)
+        }
+
+        get("{imageId?}") {
+            UserImagesController().getImage(call)
+        }
+        delete("{imageId?}") {
+            UserImagesController().deleteImage(call)
+        }
+        post("upload") {
+            UserImagesController().uploadImage(call)
+        }
+
+    }
+
+}
+
+
+
+
---
+++ src/main/kotlin/com/mobileprism/database/model/images/UserImages.kt
@@ -0,0 +1,43 @@
+package com.mobileprism.database.model.images
+
+import com.mobileprism.database.model.users.UserDTO
+import com.mobileprism.database.model.users.Users
+import org.jetbrains.exposed.dao.id.UUIDTable
+import org.jetbrains.exposed.sql.javatime.datetime
+import org.jetbrains.exposed.sql.transactions.transaction
+import java.time.LocalDateTime
+import java.util.UUID
+
+object UserImages : UUIDTable("user_images") {
+
+    internal val user = reference("user", Users)
+    internal val datetimeCreated = datetime("datetime_created").default(LocalDateTime.now())
+
+    fun addNewImage(user: UserDTO, imageUUID: UUID): UserImageDTO {
+        return transaction {
+            UserImageDTO.new(imageUUID) {
+                this.user = user
+            }
+        }
+    }
+
+    fun getAllImagesByUser(user: UserDTO): List<UserImageDTO> {
+        return transaction {
+            UserImageDTO.all().toList()
+        }
+    }
+
+    fun getImageById(imageId: UUID): UserImageDTO? {
+        return transaction {
+            UserImageDTO.findById(imageId)
+        }
+    }
+
+    fun delete(it: UserImageDTO) {
+        return transaction {
+            it.delete()
+        }
+    }
+
+}
+
